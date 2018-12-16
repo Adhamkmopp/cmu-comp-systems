abb6a8d9b11043b2bd693c2b9f53e7bf5164f36e
Adham <wbf326@alumni.ku.dk>
2018-12-16 10:54:25 +0100

    Test

---
+++ Chapter 2/Homework/2.6.3.cpp
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <iostream>
+#include <string.h>
+#include "byte.h"
+
+using namespace std;
+
+unsigned srl (unsigned x, int k){
+    unsigned xsra = (int) x >> k;
+    int size = sizeof(int) * 8;
+    return (xsra & ~(0x1 << (size -1)) >> k);
+}
+
+signed sra(int x, int k){
+    int xsrl = (unsigned) x>>k;
+    int size = sizeof(int) * 8;
+    return (xsrl | ~(0x1 << (size -1)) >> k);
+}
+
+
+int main (){
+
+
+}
